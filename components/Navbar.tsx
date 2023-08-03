fd4b6b613c54e541665b28969e91d9ab5d06bd90
Mariusz Wnęk <wnek.mariusz@gmail.com>
2023-08-03 21:45:29 +0200

    Add hover states for the navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 export default function Navbar() {
 
     return (
         <>
             <div className="nav_links_wrapper">
                 <div className='nav_links'>
-                    <a href={'../getting-started'}>Getting Started</a>
+                    <a className="selected" href={'../getting-started'}>Getting Started</a>
                     <a href={'../methodology'}>Methodology</a>
                     <a href={'../brand'}>Brand</a>
                     <a href={'../tagger-labs'}>Tagger Labs</a>
