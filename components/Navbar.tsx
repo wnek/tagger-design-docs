b48427bb5d79d8bac3dbcbbe94d12a7ebb089f28
Mariusz Wnęk <wnek.mariusz@gmail.com>
2023-08-04 08:47:20 +0200

    Add contact us link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,20 +5,22 @@ export default function Navbar() {
 
     return (
         <>
-            <div className="nav_links_wrapper">
-                <div className='nav_links'>
-                    <a className="selected" href={'../getting-started'}>Getting Started</a>
-                    <a href={'../methodology'}>Methodology</a>
-                    <a href={'../brand'}>Brand</a>
-                    <a href={'../tagger-labs'}>Tagger Labs</a>
-                </div>
-            </div>
+
             <nav>
+                <div className="nav_links_wrapper">
+                    <div className='nav_links'>
+                        <a className="selected" href={'../getting-started'}>Getting Started</a>
+                        <a href={'../methodology'}>Methodology</a>
+                        <a href={'../brand'}>Brand</a>
+                        <a href={'../tagger-labs'}>Tagger Labs</a>
+                    </div>
+                </div>
                 <div className='nav_logo'>
                     <a href={'../'}>
                         <img src={'./img/taggerLogo.svg'} />
                     </a>
                 </div>
+                <div className='help'><p>🛟Need help? <a href="mailto: mariusz@scalaric.com">Contact us</a></p></div>
 
             </nav>
         </>
