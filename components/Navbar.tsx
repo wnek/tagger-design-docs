73c2f627a23037cffd28df7083598514aa35e604
Mariusz Wnęk <wnek.mariusz@gmail.com>
2023-08-08 21:14:06 +0200

    Add mobile navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,42 @@
 'use client'
 
+
+import { useState } from 'react'
+
 import { usePathname } from 'next/navigation'
 import Link from "next/link"
+import { is } from '@react-three/fiber/dist/declarations/src/core/utils'
+
+
+
 
 export default function Navbar() {
     const currentPathname = usePathname()
     const currentPage = currentPathname.split('/')[1]
 
+    const [isOpened, setIsOpened] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpened(!isOpened)
+    }
+    console.log(isOpened)
+
     return (
         <>
             <nav>
-                <div className="nav_links_wrapper">
-                    <div className='nav_links'>
-                        <Link className={currentPathname === '/' ? "selected" : ""} href={'../'}>Getting Started</Link>
-                        <Link className={currentPage === 'philosophy' ? "selected" : ""} href={'/philosophy'}>Philosophy</Link>
-                        <Link className={currentPage === 'design-language' ? "selected" : ""} href={'/design-language'}>Design Language</Link>
-                        <Link className={currentPage === 'fenix-design-system' ? "selected" : ""} href={'/fenix-design-system'}>Fenix Design System</Link>
-                        <Link className={currentPage === 'tagger-labs' ? "selected" : ""} href={'/tagger-labs'}>Tagger Labs</Link>
+                <button className="nav_button" onClick={toggleMenu}>&times;</button>
+                <div className={`nav_links_wrapper ${isOpened ? "is-opened" : ""} `}>
+
+                    <div className={`nav_links ${isOpened ? "is-opened" : ""} `}>
+                        <Link className={currentPathname === '/' ? "selected" : ""} href={'../'} onClick={toggleMenu}>Getting Started</Link>
+                        <Link className={currentPage === 'philosophy' ? "selected" : ""} href={'/philosophy'} onClick={toggleMenu}>Philosophy</Link>
+                        <Link className={currentPage === 'design-language' ? "selected" : ""} href={'/design-language'} onClick={toggleMenu}>Design Language</Link>
+                        <Link className={currentPage === 'fenix-design-system' ? "selected" : ""} href={'/fenix-design-system'} onClick={toggleMenu}>Fenix Design System</Link>
+                        <Link className={currentPage === 'tagger-labs' ? "selected" : ""} href={'/tagger-labs'} onClick={toggleMenu}>Tagger Labs</Link>
                     </div>
+
+
+
                 </div>
             </nav>
 
